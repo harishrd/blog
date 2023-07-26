bafcdbd64ad55b94ea9c0855dbeb93e8ded6478e
harishrd <harishrd321@gmail.com>
2023-07-26 15:19:35 +0530

    update article list page to preview few article content

--- src/pages/ArticleListPage.js
+++ src/pages/ArticleListPage.js
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
+import articles from "./article-content";
+
+
 const ArticleListPage = () => { 
     return (
         <>
-            <h1>Articles</h1>
-            <div>
-                <h2>Article 1</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-
-                <h2>Article 2</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
-            </div>
+            {articles.map((article, key) => (
+                <Link key={article.name} to={`/articles/${article.name}`}>
+                    <h3>{article.title}</h3>
+                    <p>{article.content[0].substring(0, 150)}...</p>
+                </Link>
+            ))}
         </>
     );
 };
+
 
 export default ArticleListPage; 
