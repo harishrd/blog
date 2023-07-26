92114d8b218f86f55d6de38577bc46c2a6f75a4c
harishrd <harishrd321@gmail.com>
2023-07-26 15:18:33 +0530

    add dummy DB with articles

---
+++ src/pages/article-content.js
@@ -0,0 +1,54 @@
+
+let articles = [
+    {
+        name: 'learn-react',
+        title: 'The Fastest Way to Learn React',
+        content: [
+            `Welcome! Today we're going to be talking about the fastest way to learn React.
+            I'm going to be giving you a roadmap and some resources that you can use to learn React as fast as possible.
+            So, let's get started!`,
+
+            `First, let's talk about what React is. React is a JavaScript library for building user interfaces.
+            It's maintained by Facebook and a community of individual developers and companies.
+            It's used at Facebook, Instagram, Netflix, and many other companies.`,
+            `React is a JavaScript library for building user interfaces.
+
+            It's maintained by Facebook and a community of individual developers and companies.
+            It's used at Facebook, Instagram, Netflix, and many other companies.`,
+        ]
+    },
+
+    {
+        name: 'learn-node',
+        title: 'How to Build a Node Server in 10 Minutes',
+        content: [
+            `In this article, we're going to be talking about how to build a Node server in 10 minutes.
+            We're going to be using Node, Express, and a few other libraries to build a simple server that allows you to store and retrieve data from a MongoDB database.`,
+
+            `First, let's talk about what Node is. Node is a JavaScript runtime that allows you to run JavaScript on the server.
+            It's built on top of the V8 JavaScript engine, which is the same engine that powers Google Chrome.
+            It's used by companies like Netflix, Uber, and PayPal.`,
+
+            `Node is a JavaScript runtime that allows you to run JavaScript on the server.
+            It's built on top of the V8 JavaScript engine, which is the same engine that powers Google Chrome.
+            It's used by companies like Netflix, Uber, and PayPal.`,
+        ]
+    },
+
+    {
+        name: 'my-thoughts-on-resumes',
+        title: 'My Thoughts on Resumes',
+        content: [
+            `In this article, we're going to be talking about my thoughts on resumes.
+            I'm going to be giving you some tips and tricks that you can use to make your resume stand out from the crowd.`,
+            
+            `First, let's talk about what a resume is. A resume is a document that you send to potential employers to show them that you're qualified for the job.
+            It's usually one or two pages long and contains information about your education, work experience, skills, and other relevant information.`,
+
+            `A resume is a document that you send to potential employers to show them that you're qualified for the job.
+            It's usually one or two pages long and contains information about your education, work experience, skills, and other relevant information.`,
+        ]
+    }
+];
+
+export default articles;
